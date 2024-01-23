52fa388f1765eb67d8aa2b5de28e33323b591bc7
kwangnam <kncho83@gmail.com>
2024-01-23 15:57:17 +0900

    커밋

--- src/caffe_main/MainController.java
+++ src/caffe_main/MainController.java
@@ -6,82 +6,102 @@ import java.util.Scanner;
 
 public class MainController {
 
-    public static void help() {
-        System.out.println();
-        System.out.println("안녕하세요. 팀 프로젝트 카페에 오신 것을 환영합니다.");
-        System.out.println("-------------------------------------");
-        System.out.println("회원가입 - new 아이디 비밀번호");
-        System.out.println("비밀번호변경 - change 아이디 현재비밀번호 변경비밀번호");
-        System.out.println("로그인 - 아이디 비밀번호");
-        System.out.println("메뉴호출 - menu");
-        System.out.println("정보조회 - info 아이디");
-        System.out.println("프로그램종료 - exit");
-        System.out.println("도움말 - help");
-        System.out.println("-------------------------------------");
-    }
-    
-    public static void register() {
-        System.out.println();
-        System.out.println("-------------------------------------");
-        System.out.println("회원가입 - new 아이디 비밀번호");
-        System.out.println("도움말 - help");
-        System.out.println("-------------------------------------");
-    }
-    
-    public static void login() {
-    	System.out.println();
-        System.out.println("-------------------------------------");
-        System.out.println("로그인 - 아이디 비밀번호");
-        System.out.println("도움말 - help");       
-        System.out.println("-------------------------------------");
-    
-    }
+	public static void help() {
+	    System.out.println();
+	    System.out.println("안녕하세요. 팀 프로젝트 카페에 오신 것을 환영합니다.");
+	    System.out.println("-------------------------------------");
+	    System.out.println("회원가입 - new 아이디 비밀번호");
+	    System.out.println("비밀번호변경 - change 아이디 현재비밀번호 변경비밀번호");
+	    System.out.println("로그인 - 아이디 비밀번호");
+	    System.out.println("메뉴호출 - menu");
+	    System.out.println("정보조회 - info 아이디");
+	    System.out.println("프로그램종료 - exit");
+	    System.out.println("도움말 - help");
+	    System.out.println("-------------------------------------");
+	}
 
-    public static void main(String[] args) {
-        CafeMenuService cms = new CafeMenuService();
-        CafeSalesService css = new CafeSalesService();
-        UserMenuService ums = new UserMenuService();
-        UserPayService ups = new UserPayService();
+	public static void register() {
+	    System.out.println();
+	    System.out.println("-------------------------------------");
+	    System.out.println("회원가입 - new 아이디 비밀번호 비밀번호확인");
+	    System.out.println("도움말 - help");
+	    System.out.println("-------------------------------------");
+	}
 
-        Scanner sc = new Scanner(System.in);
+	public static void login() {
+	    System.out.println();
+	    System.out.println("-------------------------------------");
+	    System.out.println("로그인 - 아이디 비밀번호");
+	    System.out.println("도움말 - help");
+	    System.out.println("-------------------------------------");
 
-        System.out.println("안녕하세요. 팀 프로젝트 카페에 오신 것을 환영합니다.");
-        System.out.println("기존 멤버십 회원이신가요? (Y/N)");
-        String userInput = sc.nextLine();
-        
-        String[] userInputs = userInput.split(" ");
-        
-        if (userInput.equalsIgnoreCase("N")) {
-            register();
-            System.out.println("회원가입을 진행하여 주세요.");
-            userInput = sc.nextLine();
-            userInputs = userInput.split(" ");
-            
-            if (userInputs.length != 3) {
-            	System.out.println("입력하신 정보가 형식에 맞지 않습니다.");
-            	register();
-            	userInput = sc.nextLine();
-            	userInputs = userInput.split(" ");
-            	
+	}
 
-            }
-            
-            RequestDTO dto = new RequestDTO(userInputs[1], userInputs[2], userInputs[3]);
-        } else if(userInput.equalsIgnoreCase("Y")) {
-        	login();
-        	System.out.println("아이디와 비밀번호를 입력해주세요.");
-        	userInput = sc.nextLine();
-        	userInputs = userInput.split(" ");
-        	
-            if (userInputs.length != 2) {
-            	System.out.println("입력하신 정보가 형식에 맞지 않습니다.");
-            	login();
-            	userInput = sc.nextLine();
-            	userInputs = userInput.split(" ");
-            }
-        }
+	public static void menu() {
+	    System.out.println();
+	    System.out.println("********* 팀 프로젝트 카페 MENU *********");
+	    System.out.println("     메뉴 1. 아메리카노  3,000원      ");
+	    System.out.println("     메뉴 2. 카페라떼   3,500원      ");
+	    System.out.println("     메뉴 3. 카푸치노   1,500원      ");
+	    System.out.println("     메뉴 4. 에스프레소  2,500원      ");
+	    System.out.println("     메뉴 5. 카페모카   4,000원      ");
+	    System.out.println("     메뉴 6. 치즈케이크  5,000원      ");
+	    System.out.println("     메뉴 7. 초코케이크  4,500원      ");
+	    System.out.println("************ Thank you **************");
+	    System.out.println("주문방법 - 메뉴번호 수량 엔터 > ok 입력");
+	}
 
-        // 나머지 로직 추가
+	public static void payment() {
+	    System.out.println();
+	    System.out.println("**** 결제수단 ****");
+	    System.out.println(" 1. 현금 ");
+	    System.out.println(" 2. 카드 ");
+	    System.out.println(" 3. 삼성페이");
+	}
 
-    }
+	public static void main(String[] args) {
+	    CafeMenuService cms = new CafeMenuService();
+	    CafeSalesService css = new CafeSalesService();
+	    UserMenuService ums = new UserMenuService();
+	    UserPayService ups = new UserPayService();
+
+	    Scanner sc = new Scanner(System.in);
+
+	    System.out.println("안녕하세요. 팀 프로젝트 카페에 오신 것을 환영합니다.");
+	    System.out.println("기존 멤버십 회원이신가요? (Y/N)");
+	    String userInput = sc.nextLine();
+
+	    String[] userInputs = userInput.split(" ");
+
+	    if (userInput.equalsIgnoreCase("N")) {
+	        register();
+	        System.out.println("회원가입을 진행하여 주세요.");
+	        userInput = sc.nextLine();
+	        userInputs = userInput.split(" ");
+
+	        if (userInputs.length != 3) {
+	            System.out.println("입력하신 정보가 형식에 맞지 않습니다.");
+	            register();
+	            userInput = sc.nextLine();
+	            userInputs = userInput.split(" ");
+	        }
+
+	    } else if (userInput.equalsIgnoreCase("Y")) {
+	        login();
+	        System.out.println("아이디와 비밀번호를 입력해주세요.");
+	        userInput = sc.nextLine();
+	        userInputs = userInput.split(" ");
+	        menu();
+
+	        while (true) {
+	            userInput = sc.nextLine();
+	            if (userInput.equalsIgnoreCase("ok")) {
+	                break;
+	            }
+	        }
+	        System.out.println("결제수단을 선택하세요.");
+	        payment();
+	    }
+	}
+	// 나머지 로직 추가
 }
---
+++ src/caffe_main/m.java
@@ -0,0 +1,10 @@
+package caffe_main;
+
+public class m {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
